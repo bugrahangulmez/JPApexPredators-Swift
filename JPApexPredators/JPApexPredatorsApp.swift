d5d60cfcd728f81914fd605b713b0f38afcd6776
bugrahan gulmez <114365074+bugrahangulmez@users.noreply.github.com>
2024-10-28 00:42:14 +0300

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Bugrahan on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
