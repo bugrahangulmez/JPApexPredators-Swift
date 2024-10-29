640850e2b405ff77ae37dabe81ef2f073a350c77
bugrahan gulmez <114365074+bugrahangulmez@users.noreply.github.com>
2024-10-29 16:21:47 +0300

    ss

---
+++ JPApexPredators/ApexPredator.swift
@@ -0,0 +1,8 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Bugrahan on 29.10.2024.
+//
+
+import Foundation
---
+++ JPApexPredators/Predators.swift
@@ -0,0 +1,8 @@
+//
+//  Predators.swift
+//  JPApexPredators
+//
+//  Created by Bugrahan on 29.10.2024.
+//
+
+import Foundation
